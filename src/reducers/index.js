74712f6466249cb9ae4c974dbc460107bc4f4a84
Amiry <amirygames@gmail.com>
2017-09-06 07:37:40 +0300

    empty reducer removed

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,9 +1,7 @@
 import { combineReducers } from 'redux';
-import interfaceReducer from './interfaceReducer';
 import gameReducer from './gameReducer';
 
 const allReducers = combineReducers({
-    interface: interfaceReducer,
     game: gameReducer
 });
 
